483c5346b3ced0fad945426a743920423400d25d
Raquel Álvarez <ralvhdez@gmail.com>
2016-12-02 15:12:25 +0100

    footer fixed

--- admin/footer.php
+++ admin/footer.php
@@ -1,14 +1,12 @@
 
 		</div>
 		<br>
-		<div class="foot" id="footer">
-			<div class="container">
-				<p class="text-muted">
-					<a href="./../index.php" target="_blank" title="e-Cosecha">e-Cosecha</a> | Copyright 2016. All rights reserved. 										
-				</p>
-
-			</div>
-		</div>
+		<footer class="footer">
+      <div class="container">
+        <p class="text-muted">
+						<a href="./../index.php" target="_blank" title="e-Cosecha">e-Cosecha</a> | Copyright 2016. All rights reserved. 						</p>
+      </div>
+    </footer>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src= "js/mensajes.js" type = "text/javascript"></script>
--- footer.php
+++ footer.php
@@ -1,14 +1,13 @@
 
 		</div>
-		<br>
-		<div class="foot" id="footer">
-			<div class="container">
-				<p class="text-muted">
-					<a href="index.php">e-Cosecha</a> | <a href="privacity.php">Privacidad</a> | <a href="aviso-legal.php">Aviso Legal</a>  | Copyright 2016. All rights reserved. 										
+		<br>	
+		<footer class="footer">
+      <div class="container">
+        <p class="text-muted">
+					<a href="index.php">e-Cosecha</a> | <a href="privacity.php">Privacidad</a> | <a href="aviso-legal.php">Aviso Legal</a>  | Copyright 2016. All rights reserved.
 				</p>
-
-			</div>
-		</div>
+      </div>
+    </footer>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 
